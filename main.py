46484a09516e493fad4033f9bb0323d3691679c1
lukmac290 <lukamac290@student.polsl.pl>
2022-10-26 14:15:46 +0200

    lkasjnf

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from typing import Union
+from fastapi import FastAPI
+
 from services.PrimeNumberService import checkPrimeNumber
 from services.ImageService import InvertPictureColors
 
-#zad1
-data = input("Provide any positive number: ")
-checkPrimeNumber(data)
+app = FastAPI()
+# W folderze z mainem w cmd 'uvicorn main:app --reload'
 
+#zad1
+@app.get("/primeverif/{number}")
+def prime(number: str):
+    response = {
+        "Number": number,
+        "Prime": checkPrimeNumber(number)
+    }
+    return response
 #zad2
-InvertPictureColors('kot.jpg')
+# InvertPictureColors('kot.jpg')
